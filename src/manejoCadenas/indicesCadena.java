bd1fec238034c4399f9d7d5a47cae26b86d22f4a
Amilcar <raclima977@gmail.com>
2025-12-15 21:44:16 -0500

    El segundo commit

--- src/manejoCadenas/indicesCadena.java
+++ src/manejoCadenas/indicesCadena.java
@@ -1,4 +1,19 @@
 package manejoCadenas;
 
 public class indicesCadena {
+    public static void main(String[] args) {
+        // Manejo de indices en una cadena
+        var cadena1 = "Hola Mundo";
+
+
+
+
+        //Recuperar el primer caracter
+        var primerCaracter = cadena1.charAt(0);
+        System.out.println(primerCaracter);
+        // Recuperar el último caracter en el la posición: 9
+        var ultimoCaracter = cadena1.charAt(9);
+        System.out.println(ultimoCaracter);
+
+    }
 }
